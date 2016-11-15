36975872f1cef0baf1092f725e6645cfe86853e4
Julian <julian@iris.tv>
2016-11-14 16:27:20 -0800

    updated to account for pagination and request rate throttle

--- wistia_export.js
+++ wistia_export.js
@@ -1,11 +1,38 @@
 $(function() {
 
 	// GLOBALS
-	var api_key = "825ab49ccbff4ab140e524cbb15918a285bf98e4b1ab44e6617a0574c1d0052a";
+	var start_date,end_date;
 	var debug = false;
+	var num_requests_made = 0;
+	var media_ids = {};
+	var visitor_ids = {};
+	var events = [];
+	var eventDates = {};  // used to check duplicate events
+	var page = 1;
+
+	// SETTINGS	
+	var api_key = "7ea1206fc22043174d677592dad08c53488821253376ef1adad77c75a9b586e1";
+	var NUM_SECONDS_TO_WAIT = 70;
+	var MILLISECONDS_IN_A_MINUTE = 1000;
+	var WAIT_TIME = MILLISECONDS_IN_A_MINUTE*NUM_SECONDS_TO_WAIT;
+	var NUM_REQUESTS_BEFORE_WAITING = 90; // rate allows for 100 but let's be safe so the account doesn't get blocked
+	var NUM_REQUESTS_BEFORE_EXPORT = 20;  // this allows for 100 * 20 rows of events per file.  Let's keep it small so the network doesn't error out on download
+
+
+	// HELPER FUNCTIONS
+	function randomString(){
+	    var text = "";
+	    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+	    for( var i=0; i < 5; i++ ){
+	        text += possible.charAt(Math.floor(Math.random() * possible.length));
+	    }
+
+	    return text;
+	}
 
 	function debugLog(message){
-
+		console.log(message);
 	}
 
 	function updateStatus(message) {
@@ -50,9 +77,150 @@ $(function() {
 		document.getElementById("action_buttons").appendChild(a);									
     }
 
-	function executeExport(start_date,end_date){
+    function appendDownloadButton(){
+		updateStatus("Converting Events To CSV");
+		csv = convertToCSV(events);
+
+		//////////////////////////////////////
+		// Convert to CSV
+		//////////////////////////////////////
+		updateStatus("Converting Events To CSV");
+		var firstFileDate = events[0].received_at;
+		var lastFileDate = events[events.length-1].received_at;
+		var fileName = firstFileDate + "->" + lastFileDate + "_events_export_download-"+randomString()+".csv";
+		csv = convertToCSV(events);
+		events = [];  // reset events
+
+		//////////////////////////////////////
+		// Append Download CSV Button
+		//////////////////////////////////////
+		updateStatus("Ready For Download");
+		appendCSVDownloadButton(csv,fileName);
+
+    }
+
+    function updateEvents(eventObject,visitor){
+ 		var formattedEvent = {
+			// event stuff
+		    "received_at" : eventObject["received_at"],
+		    "ip" : eventObject["ip"],
+		    "country" : eventObject["country"],
+		    "region" : eventObject["region"],
+		    "city" : eventObject["city"],
+		    "lat" : eventObject["lat"],
+		    "lon" : eventObject["lon"],
+		    "percent_viewed" : eventObject["percent_viewed"],
+		    "embed_url" : eventObject["embed_url"],
+		    "media_id":eventObject["media_id"],
+		    "media_name":eventObject["media_name"],
+		    "media_url":eventObject["media_url"],
+		    "email":eventObject["email"],
+		};
+
+		// add visitor stuff
+		if (visitor){
+			formattedEvent.visitor_name= visitor["visitor_identity"]["name"],
+			formattedEvent.visitor_email= visitor["visitor_identity"]["email"],
+			formattedEvent.org_name= visitor["visitor_identity"]["org"]["name"],
+			formattedEvent.org_title= visitor["visitor_identity"]["org"]["title"],
+			formattedEvent.browser= visitor["user_agent_details"]["browser"],
+			formattedEvent.browser_version= visitor["user_agent_details"]["browser_version"],
+			formattedEvent.platform= visitor["user_agent_details"]["platform"],
+			formattedEvent.mobile= visitor["user_agent_details"]["mobile"]
+		} else {
+			formattedEvent.visitor_name= null;
+			formattedEvent.visitor_email= null;
+			formattedEvent.org_name= null;
+			formattedEvent.org_title= null;
+			formattedEvent.browser= null;
+			formattedEvent.browser_version= null;
+			formattedEvent.platform= null;
+			formattedEvent.mobile= null;
+		}
+
+		if ((eventDates[formattedEvent.received_at]) || (eventDates[formattedEvent.received_at]==formattedEvent.received_at) ){
+			debugLog("Repeated Data.  Removing...");
+		} else {
+			debugLog(formattedEvent.received_at);
+			eventDates[formattedEvent.received_at] = formattedEvent.ip;
+			events.push(formattedEvent);
+		}
+		updateStatus("Formatted Events Page: " + page);   	
+    }
+
+    function getEvents(){
+    	// Append Download Button
+		if (num_requests_made==NUM_REQUESTS_BEFORE_EXPORT)	{
+			appendDownloadButton();
+		}
+
+		// Request more events if below rate limit
+    	if (num_requests_made<NUM_REQUESTS_BEFORE_WAITING){
+
+			//////////////////////////////////////
+			// Get Events
+			//////////////////////////////////////
+			jQuery.ajax({
+				data: {
+					"start_date":start_date,
+					"end_date":end_date,
+					"per_page":100,
+					"page":page,
+					"sort_by":"created",
+					"sort_direction":1
+				},
+	            url: "https://api.wistia.com/v1/stats/events.json?api_password=" + api_key,
+	            success: function(data) {
+	            	// Increase Number of Requests made
+	            	num_requests_made+=1;
+					updateStatus("Downloaded Events.  Page: " + page);
+
+					// Keep track of previous number of events
+					var previous_events_length = events.length;
+
+					//////////////////////////////////////
+					// Store Events in Hash for Later use
+					//////////////////////////////////////
+					for (var index=0; index<data.length;index++){
+						var eventObject = data[index];
+						var visitor_key = data[index]["visitor_key"];
+						var visitor = visitor_ids[visitor_key];
+						updateEvents(eventObject,visitor);
+						
+					}
+
+					// IFF no events were added
+					if (previous_events_length!=events.length){   
+						// Increase current page count
+						page += 1;
+						debugLog("page!!!!!!!!!! " + page);
+
+						// Process new page events
+						getEvents();
+					} else {
+						// we're out of events to process!  Let's download 
+						appendDownloadButton();
+					}
+	            }
+	        }).fail(function(){
+	        	updateStatus("Export Failed: Error Retrieving Events");
+	        });     	
+    	} else {
+    		updateStatus("Reached Request Limit.  Waiting 60 seconds....")
+    		// wait for 60 seconds	
+			setTimeout(function(){
+				// Reset number of requests made
+				num_requests_made = 0;
+
+				// Process events
+				getEvents();
+			}, WAIT_TIME);
+    	}
+    }
+
+	function executeExport(){
 		updateStatus("Retrieving Selected Dates");
-		debugLog(start_date + " -> " + end_date);
+		// debugLog(start_date + " -> " + end_date);
 
 		updateStatus("Triggered Export");
 
@@ -62,20 +230,15 @@ $(function() {
 		jQuery.ajax({
             url: "https://api.wistia.com/v1/medias.json?api_password=" + api_key,
             success: function(data) {
-				debugLog("Downloaded Medias");
-
+            	num_requests_made+=1;
 
 				//////////////////////////////////////
 				// Store Medias in Hash for Later use
 				//////////////////////////////////////
-				var media_ids = {};
 				for (var index=0; index<data.length;index++){
 					var mediaObject = data[index];
 					media_ids[mediaObject["hashed_id"]] = mediaObject;
 				}
-				debugLog(media_ids);
-
-
 
 				//////////////////////////////////////
 				// Get Visitors
@@ -83,86 +246,18 @@ $(function() {
 				jQuery.ajax({
 		            url: "https://api.wistia.com/v1/stats/visitors.json?api_password=" + api_key,
 		            success: function(data) {
+		            	num_requests_made+=1;
 						updateStatus("Downloaded Visitors");
-
 
 						//////////////////////////////////////
 						// Store Visitors in Hash for Later use
 						//////////////////////////////////////
-						var visitor_ids = {};
 						for (var index=0; index<data.length;index++){
 							var visitorObject = data[index];
 							visitor_ids[visitorObject["visitor_key"]] = visitorObject;
 						}
-						debugLog(visitor_ids);
 
-
-							//////////////////////////////////////
-							// Get Events
-							//////////////////////////////////////
-							jQuery.ajax({
-								data: {
-									"start_date":start_date,
-									"end_date":end_date
-								},
-					            url: "https://api.wistia.com/v1/stats/events.json?api_password=" + api_key,
-					            success: function(data) {
-									updateStatus("Downloaded Events");
-
-									
-									//////////////////////////////////////
-									// Store Events in Hash for Later use
-									//////////////////////////////////////
-									var events = [];
-									for (var index=0; index<data.length;index++){
-										var eventObject = data[index];
-										var visitor_key = data[index]["visitor_key"];
-										var visitor = visitor_ids[visitor_key];
-										events.push({
-											// event stuff
-										    "received_at" : eventObject["received_at"],
-										    "ip" : eventObject["ip"],
-										    "country" : eventObject["country"],
-										    "region" : eventObject["region"],
-										    "city" : eventObject["city"],
-										    "lat" : eventObject["lat"],
-										    "lon" : eventObject["lon"],
-										    "percent_viewed" : eventObject["percent_viewed"],
-										    "embed_url" : eventObject["embed_url"],
-
-										    // add visitor stuff
-											"visitor_name": visitor["visitor_identity"]["name"],
-											"visitor_email": visitor["visitor_identity"]["email"],
-											"org_name": visitor["visitor_identity"]["org"]["name"],
-											"org_title": visitor["visitor_identity"]["org"]["title"],
-											"browser": visitor["user_agent_details"]["browser"],
-											"browser_version": visitor["user_agent_details"]["browser_version"],
-											"platform": visitor["user_agent_details"]["platform"],
-											"mobile": visitor["user_agent_details"]["mobile"]
-										});
-										updateStatus("Formatted Events");
-									}
-									debugLog(events);
-
-									//////////////////////////////////////
-									// Conver to CSV
-									//////////////////////////////////////
-									updateStatus("Converting Events To CSV");
-									csv = convertToCSV(events);
-									debugLog(csv);
-
-									//////////////////////////////////////
-									// Append Download CSV Button
-									//////////////////////////////////////
-									updateStatus("Ready For Download");
-									var fileName = start_date + "->" + end_date + "_events_export_download";
-									appendCSVDownloadButton(csv,fileName);
-
-					            }
-					        }).fail(function(){
-					        	updateStatus("Export Failed: Error Retrieving Events");
-					        });  
-
+						getEvents();
 		            }
 		        }).fail(function(){
 		        	updateStatus("Export Failed: Error Retrieving Visitors");
@@ -184,9 +279,11 @@ $(function() {
 	});
 
 	$("#daterangepicker").on('apply.daterangepicker', function(ev, picker) {
-		var start_date = picker.startDate.format('YYYY-MM-DD');
-		var end_date = picker.endDate.format('YYYY-MM-DD');
-			executeExport(start_date,end_date);
+		start_date = picker.startDate.format('YYYY-MM-DD');
+		end_date = picker.endDate.format('YYYY-MM-DD');
+		$("#start_date_display").text("Start Date: " + start_date);
+		$("#end_date_display").text("End Date: " + end_date);
+		executeExport();
 	});
 
 	$("#daterangepicker").on('cancel.daterangepicker', function(ev, picker) {
